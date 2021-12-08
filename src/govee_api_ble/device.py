33ec6d73d13ab9927cfaabc4fb92c89414e8018b
softgrass <26261205+softgrass@users.noreply.github.com>
2021-12-07 21:46:55 -0500

    Added more features

--- src/govee_api_ble/device.py
+++ src/govee_api_ble/device.py
@@ -1,5 +1,21 @@
 import os
 
+scenes = {
+	'sunrise': '3305040000000000000000000000000000000032',
+	'sunset': '3305040100000000000000000000000000000033',
+	'movie': '3305040400000000000000000000000000000036',
+	'dating': '3305040500000000000000000000000000000037',
+	'romantic': '3305040700000000000000000000000000000035',
+	'blinking': '330504080000000000000000000000000000003a',
+	'candlelight': '330504090000000000000000000000000000003b',
+	'snowflake': '3305040f0000000000000000000000000000003d',
+}
+music = {
+	'energic': '3305010000000000000000000000000000000037',
+	'spectrum': '3305010100RRBBGG0000000000000000000000c9',
+	'rhythm': '3305010300000000000000000000000000000034',
+}
+
 class GoveeDevice:
 	def __init__(self, mac):
 		"""
@@ -66,3 +82,37 @@ class GoveeDevice:
 		cs_p = cs_p.replace(" ","") + level_xor_h.replace("0x","")
 		cs_p = cs_p.upper()
 		os.system("gatttool -i hci0 -b {} --char-write-req -a 0x0015 -n {}".format(self.mac,cs_p))
+	def setScene(self,setting):
+		"""
+		Sets the different scenes for the lights
+		The looks of them can be found in the Govee app
+		setting - Takes a string, type which mode you want (can be found in app or inside the 'scenes' list at the top of the code
+		Usage - my_device.setScene("setting")
+		"""
+		os.system("gatttool -i hci0 -b {} --char-write-req -a 0x0015 -n {}".format(self.mac,scenes[setting.lower()]))
+
+	def setColorMusic(self,setting,c=[255,0,0]):
+		"""
+		Sets the music mode for the lights
+		The settings for this can be found in the Govee app or in the 'music' list at the top of the code
+		Spectrum mode accepts a list of RGB values after the setting
+		setting - Takes a string, type which mode you want
+		c - Only used for Spectrum setting, accepts 3 int values as RGB values
+		Usage - my_device.setColorMusic("setting")
+				my_device.setColorMusic("spectrum",[R,G,B])
+		"""
+		if not isinstance(c, list) or len(c) is not 3:
+			raise TypeError
+		for v in c:
+			if not isinstance(v,int):
+				raise TypeError
+			if not (v >= 0 and v <= 255):
+				raise ValueError
+		hex_c = [hex(v) if len(hex(v)) is 4 else hex(v)[0:2]+"0"+hex(v)[2:] for v in c]
+		hex_x = [v.replace("0x","") for v in hex_c]
+		rgb_c = ''.join(hex_x)
+		pre_setting = music[setting.lower()]
+		cur_setting = (pre_setting[:10] + rgb_c + pre_setting[16:]) if("RR" in pre_setting) else pre_setting
+		print(cur_setting)
+		os.system("gatttool -i hci0 -b {} --char-write-req -a 0x0015 -n {}".format(self.mac,cur_setting))
+
